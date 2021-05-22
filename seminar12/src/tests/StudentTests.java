b3ec968674949d92477b63c1b57e3c0d23b890ae
Ceddy <radumaria18@stud.ase.ro>
2021-05-22 11:06:09 +0300

    Testare Unitara

--- seminar12/src/tests/StudentTests.java
+++ seminar12/src/tests/StudentTests.java
@@ -36,4 +36,65 @@ public class StudentTests {
 		student.adaugaNota(nota);
 		assertEquals(nota, student.getNota(0));
 	}
+	
+	@Test
+	public void testDimensiuneLista() {
+		Student student = new Student();
+		student.adaugaNota(10);
+		student.adaugaNota(8);
+		assertEquals(2, student.getNote().size());
+	}
+	
+	@Test 
+	public void testCalculeazaMedie() {
+		Student student = new Student();
+		student.adaugaNota(10);
+		student.adaugaNota(9);
+		assertEquals(9.5f, student.calculeazaMedie(),0.01); //0.1 (delta) marja de err
+	}
+	
+	@Test
+	public void testStudentCuRestanta() {
+		Student student = new Student();
+		student.adaugaNota(4);
+		//assertEquals(true, student.areRestante()); 
+		assertTrue(student.areRestante());
+	}
+	
+	@Test
+	public void testStudentFaraRestanta() {
+		Student student = new Student();
+		student.adaugaNota(5);
+		//assertEquals(false, student.areRestante());
+		assertFalse(student.areRestante());
+	}
+	
+	
+	@Test
+	public void testGetNotaIndexInvald() {   //specific JUnit 3
+		Student student = new Student();
+		student.adaugaNota(10);
+		//1 - se exe
+		try {
+			//2 - se incearca exe
+			student.getNota(-1);
+			//3 - nu se ajunge
+			fail("Nu trebuia sa ajunga aici. Mtd nu arunca exceptie");
+		}catch(IndexOutOfBoundsException exception){
+			//4 - se exe
+			
+		}catch(Exception exception) {
+			//5 - nu se ajunge
+			fail("Nu trebuia sa ajunga aici. Tipul exceptie aruncate nu este corect");
+		}
+		//6 - se exe
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class) // verifica daca una din mtd apelate arunca exceptia
+	public void testGetNotaIndexInvalidJUnit4() {
+		Student student = new Student();
+		student.adaugaNota(10);
+		student.getNota(-1);
+	}
+	//JUnit 5 - are un assert throws (lambda expressions)
 }
